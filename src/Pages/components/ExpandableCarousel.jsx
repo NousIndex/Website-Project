2bd14bbef11b7f59a01912c8bd580604b3e4e723
XiangHui556 <2101993@sit.singaporetech.edu.sg>
2023-09-27 14:49:29 +0800

    Added CSS animation to Carousel

--- src/Pages/components/ExpandableCarousel.jsx
+++ src/Pages/components/ExpandableCarousel.jsx
@@ -1,93 +1,91 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './CSS/ExpandableCarousel.css';
 import Modal from 'react-modal';
-import { CSSTransition } from 'react-transition-group';
+import 'animate.css/animate.min.css';
 
 const ExpandableCarousel = ({ items }) => {
-  const [expandedIndex, setExpandedIndex] = useState(null); // Index of the expanded item
-  const [modalIsOpen, setModalIsOpen] = useState(false); // State to control the modal
-  const nodeRef = useRef(null);
+  const [expandedIndex, setExpandedIndex] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+  const [selectedModalIndex, setSelectedModalIndex] = useState(0); // Initialize to 0
 
-  // Function to open modal
   const openModal = (index) => {
     setExpandedIndex(index);
+    setSelectedModalIndex(index); // Set the selected index when opening the modal
     setModalIsOpen(true);
+    setIsClosing(false); // Reset the closing state
   };
 
-  // Function to close modal
   const closeModal = () => {
-    setExpandedIndex(null);
-    setModalIsOpen(false);
+    setIsClosing(true); // Trigger the closing animation
+    // Delay closing the modal to allow the animation to complete
+    setTimeout(() => {
+      setExpandedIndex(null);
+      setModalIsOpen(false);
+      setIsClosing(false);
+    }, 275); // Adjust the delay time to match your animation duration
   };
+
+  // Define the modal animation class based on the modal state
+  const modalAnimationClass = modalIsOpen
+    ? isClosing
+      ? 'animate__zoomOut' // Use a fade-out animation when closing
+      : 'animate__zoomIn'
+    : '';
 
   return (
     <div className="carousel-container">
-    <Carousel 
-      showThumbs={false} 
-      autoPlay={true} 
-      interval={5000} 
-      infiniteLoop={true}
-      stopOnHover={true}
-      swipeable={true}
-      emulateTouch={true}
+      <Carousel
+        showThumbs={false}
+        autoPlay={true}
+        interval={5000}
+        infiniteLoop={true}
+        stopOnHover={true}
+        swipeable={true}
+        emulateTouch={true}
       >
-      {items.map((item, index) => (
-        <div
-          key={index}
-          className={`carousel-item`}
-          onClick={() => openModal(index)}
-        >
-          <img src={item.imageUrl} alt={`Carousel Item ${index + 1}`} />
-          <div className="carousel-legend">
-            {item.legend}
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className={`carousel-item`}
+            onClick={() => openModal(index)}
+          >
+            <img src={item.imageUrl} alt={`Carousel Item ${index + 1}`} />
+            <div className="carousel-legend">{item.legend}</div>
           </div>
-        </div>
-      ))}
-    </Carousel>
-    <CSSTransition
-        in={modalIsOpen}
-        nodeRef={nodeRef}
-        timeout={300}
-        classNames="modal"
-        unmountOnExit
+        ))}
+      </Carousel>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Image Modal"
+        className={`carousel-modal animate__animated ${modalAnimationClass}`}
+        overlayClassName="carousel-overlay"
       >
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          contentLabel="Image Modal"
-          className="carousel-modal"
-          overlayClassName="carousel-overlay"
-        >
-            <div className={`carousel-modal-content`}
-            ref={nodeRef}>
-              <Carousel 
-                showThumbs={true} 
-                infiniteLoop={true}
-                swipeable={true}
-                emulateTouch={true}
-                >
-                {items.map((item, index) => (
-                  <div
-                    key={index}
-                    className={`carousel-modal-item`}
-                    onClick={() => closeModal(index)}
-                  >
-                    <img src={item.imageUrl} alt={`Carousel Item ${index + 1}`} />
-                    <div className="carousel-modal-legend">
-                      {item.legend}
-                    </div>
-                  </div>
-                ))}
-              </Carousel>
-              {/* <img src={items[expandedIndex].imageUrl} alt={`carousel-Carousel Item ${expandedIndex + 1}`} onClick={closeModal} />
-              <div className="carousel-legend-modal">
-                {items[expandedIndex].legend}
-              </div> */}
-            </div>
-        </Modal>
-      </CSSTransition>
+        <div className={`carousel-modal-content`}>
+          <Carousel
+            showThumbs={true}
+            infiniteLoop={true}
+            swipeable={true}
+            emulateTouch={true}
+            selectedItem={selectedModalIndex} // Set the default selected item
+          >
+            {items.map((item, index) => (
+              <div
+                key={index}
+                className={`carousel-modal-item`}
+                onClick={() => closeModal(index)}
+              >
+                <img src={item.imageUrl} alt={`Carousel Item ${index + 1}`} />
+                <div className="carousel-modal-legend">{item.legend}</div>
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      </Modal>
     </div>
   );
 };
