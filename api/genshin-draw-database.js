0dcb938afd45da025e30a279a427e6ce5b7b764c
XiangHui <92633730+XiangHui556@users.noreply.github.com>
2023-10-11 18:20:18 +0800

    Vercel test udpate 1.0.12

---
+++ api/genshin-draw-database.js
@@ -0,0 +1,136 @@
+const cheerio = require('cheerio');
+
+module.exports = async (req, res) => {
+  console.log('Starting Genshin Database API');
+  // Define the URL of the MediaWiki API
+  const apiUrl = 'https://genshin-impact.fandom.com/api.php';
+
+  // Define the parameters for your API request
+  const params = {
+    action: 'parse', // You can use 'parse' to retrieve page content
+    page: 'Weapon/List', // The page you want to fetch data from
+    format: 'json', // You can specify the format as JSON
+  };
+  // Define the URL of the MediaWiki API
+  const apiUrl2 = 'https://genshin-impact.fandom.com/api.php';
+
+  // Define the parameters for your API request
+  const params2 = {
+    action: 'parse', // You can use 'parse' to retrieve page content
+    page: 'Character/List', // The page you want to fetch data from
+    format: 'json', // You can specify the format as JSON
+  };
+  try {
+    // Construct the API URL with parameters
+    const url = `${apiUrl}?${new URLSearchParams(params).toString()}`;
+    // Construct the API URL with parameters
+    const url2 = `${apiUrl2}?${new URLSearchParams(params2).toString()}`;
+
+    // Fetch data from the API
+    const response = await fetch(url);
+    const data = await response.json();
+    // Fetch data from the API
+    const response2 = await fetch(url2);
+    const data2 = await response2.json();
+
+    // Extract the content from the response
+    const content = data.parse.text['*'];
+    // Extract the content from the response
+    const content2 = data2.parse.text['*'];
+
+    // Parse the HTML content using cheerio
+    const $ = cheerio.load(content);
+    // Parse the HTML content using cheerio
+    const $2 = cheerio.load(content2);
+
+    const tableRow = $('tr');
+    const tableRow2 = $2('tr');
+    const extractedDataCharacter = [];
+    const extractedDataWeapon = [];
+
+    tableRow.each((index, element) => {
+      const rowData = $(element).find('td');
+
+      const column2Text = rowData.eq(1).find('a').text();
+      const column3DataSrc = rowData.eq(2).find('img').attr('data-src');
+      const column4Text = rowData.eq(3).text();
+      let column5Text = rowData.eq(4).text();
+      const column6Text = rowData.eq(5).text();
+
+      if (column2Text && column2Text !== 'Prized Isshin Blade') {
+        const existingData = extractedDataWeapon.find(
+          (data) => data.name === column2Text
+        );
+
+        if (column5Text.toLowerCase().includes('elemental mastery')) {
+          column5Text = column5Text.replace('Elemental Mastery', 'EM');
+        }
+        if (column5Text.toLowerCase().includes('physical dmg bonus')) {
+          column5Text = column5Text.replace('Physical DMG Bonus', 'Phys DMG');
+        }
+        if (column5Text.toLowerCase().includes('energy recharge')) {
+          column5Text = column5Text.replace('Energy Recharge', 'ER');
+        }
+
+        if (!existingData) {
+          const data = {
+            name: column2Text,
+            rarity: column3DataSrc.split('.png')[0] + '.png',
+            atk: column4Text,
+            sub: column5Text,
+            passive: column6Text,
+          };
+
+          extractedDataWeapon.push(data);
+        }
+      }
+    });
+
+    tableRow2.each((index, element) => {
+      const rowData = $(element).find('td');
+
+      const column2Text = rowData.eq(1).find('a').text().trim();
+      const column3DataSrc = rowData.eq(2).find('img').attr('data-src');
+      const column4DataSrc = rowData.eq(3).find('a img').attr('data-src');
+      const column5DataSrc = rowData.eq(4).find('a img').attr('data-src');
+      const column8Text = rowData.eq(7).text().trim();
+
+      if (column8Text && column2Text && column2Text !== 'Traveler') {
+        const existingData = extractedDataCharacter.find(
+          (data) => data.name === column2Text
+        );
+
+        if (!existingData) {
+          const data = {
+            name: column2Text,
+            rarity: column3DataSrc.split('.png')[0] + '.png',
+            element: column4DataSrc.split('.svg')[0] + '.svg',
+            weapon: column5DataSrc.split('.png')[0] + '.png',
+            release: column8Text,
+          };
+          extractedDataCharacter.push(data);
+        }
+      }
+    });
+    // Create an array of character objects with keys and release dates
+    const characterArray = Object.entries(extractedDataCharacter).map(
+      ([name, data]) => ({
+        name: data.name,
+        rarity: data.rarity,
+        element: data.element,
+        weapon: data.weapon,
+        release: data.release,
+      })
+    );
+
+    // Sort the characterArray by release date (newest first)
+    characterArray.sort((a, b) => new Date(b.release) - new Date(a.release));
+
+    res.json({
+      characters: characterArray,
+      weapons: extractedDataWeapon,
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
---
+++ api/genshin-draw-import.js
@@ -0,0 +1,212 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+
+
+module.exports = async (req, res) => {
+  console.log('Starting Genshin Draw Import API');
+  try {
+    let endid = '0';
+    let banner = 100;
+    const authkey = req.query.authkey;
+    const userID = req.query.userID;
+    const newDraws = [];
+    let loop = true;
+    let genshin_uid = '';
+
+    while (loop) {
+      const apiUrl =
+        'https://hk4e-api-os.hoyoverse.com/event/gacha_info/api/getGachaLog?authkey_ver=1&sign_type=2&auth_appid=webview_gacha&init_type=' +
+        banner +
+        '&lang=en&authkey=' +
+        authkey +
+        '&gacha_type=' +
+        banner +
+        '&page=1&size=20&end_id=' +
+        endid;
+
+      // Use the built-in fetch to make the request to Mihoyo API
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      // Parse the response body as JSON
+      const responseData = await response.json();
+      //console.log('Response data:', responseData);
+      if (responseData.retcode === -110) {
+        // Visit API too frequently
+        // Wait for 1 seconds before trying again
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        continue;
+      }
+      if (!responseData.data) {
+        // console.log(responseData)
+        return res.json({ message: responseData.message });
+      }
+      const itemList = responseData.data.list;
+      if (itemList.length > 0) {
+        // No more data
+
+        let duplicateFound = false;
+
+        for (const item of itemList) {
+          try {
+            genshin_uid = item.uid;
+            const existingItem = await prisma.Genshin_Draw.findUnique({
+              where: {
+                DrawID: item.id, // Assuming DrawID uniquely identifies an item
+              },
+            });
+
+            if (existingItem) {
+              // console.log(
+              //   `Item with DrawID ${item.id} exists in Genshin_Draw table.`
+              // );
+              duplicateFound = true;
+              //console.log('Duplicate found');
+              break; // Exit the loop if a duplicate is found
+            } else {
+              switch (item.gacha_type) {
+                case '100':
+                  item.gacha_type = 'Beginner Wish';
+                  break;
+                case '200':
+                  item.gacha_type = 'Permanent Wish';
+                  break;
+                case '301':
+                  item.gacha_type = 'Character Event Wish';
+                  break;
+                case '400':
+                  item.gacha_type = 'Character Event Wish - 2';
+                  break;
+                case '302':
+                  item.gacha_type = 'Weapon Event Wish';
+                  break;
+                default:
+                  item.gacha_type = 'Unknown';
+              }
+
+              if (item.gacha_type !== 'Unknown') {
+                // Split the date and time string into its components
+                const [datePart, timePart, ampm] = item.time.split(' ');
+
+                // Split the date components into year, month, and day
+                const [year, month, day] = datePart.split('-').map(Number);
+
+                // Split the time components into hours, minutes, seconds, and AM/PM
+                const [time] = timePart.split(' ');
+                const [hours, minutes, seconds] = time.split(':').map(Number);
+
+                // Create the Date object
+                const dateTime = new Date(
+                  year,
+                  month - 1,
+                  day,
+                  hours,
+                  minutes,
+                  seconds
+                );
+
+                newDraws.push({
+                  Genshin_UID: item.uid,
+                  DrawID: item.id,
+                  DrawTime: dateTime,
+                  DrawType: item.gacha_type,
+                  Item_Name: item.name,
+                  Rarity: item.rank_type,
+                });
+              }
+
+              // console.log(
+              //   `Item with DrawID ${item.id} does not exist in Genshin_Draw table.`
+              // );
+            }
+          } catch (error) {
+            console.error(`Error checking item with DrawID ${item.id}:`, error);
+          }
+        }
+        endid = itemList[itemList.length - 1].id;
+        if (duplicateFound) {
+          if (banner === 100) {
+            banner = 301;
+            endid = '0';
+          } else if (banner === 301) {
+            banner = 400;
+            endid = '0';
+          } else if (banner === 400) {
+            banner = 302;
+            endid = '0';
+          } else if (banner === 302) {
+            banner = 200;
+            endid = '0';
+          } else {
+            loop = false;
+          }
+        }
+      } else {
+        if (banner === 100) {
+          // get uid
+          banner = 301;
+          endid = '0';
+        } else if (banner === 301) {
+          banner = 400;
+          endid = '0';
+        } else if (banner === 400) {
+          banner = 302;
+          endid = '0';
+        } else if (banner === 302) {
+          banner = 200;
+          endid = '0';
+        } else {
+          loop = false;
+        }
+      }
+    }
+    // console.log(newDraws);
+    // console.log(userID);
+
+    // Use Prisma to update the Genshin_User table with the new UID
+    await prisma.Games_Users.upsert({
+      where: { UID: userID },
+      update: { Genshin_UID: genshin_uid },
+      create: { UID: userID, Genshin_UID: genshin_uid },
+    });
+
+    // Use Prisma to create a new entry in the Genshin_Draw table
+    if (newDraws.length > 0) {
+      await prisma.Genshin_Draw.createMany({
+        data: newDraws,
+        skipDuplicates: true,
+      });
+      // Update SummaryTable about the new data
+
+      // Calculate the total number of items in newDraws
+      const totalItems = newDraws.length;
+
+      // Update the SummaryTable with the new total item count
+      await prisma.SummaryTable.upsert({
+        where: { Game_UID: `Genshin-${newDraws[0].Genshin_UID}` },
+        update: {
+          total_items: {
+            increment: totalItems, // Specify the amount to increment by
+          },
+        },
+        create: {
+          Game_UID: `Genshin-${newDraws[0].Genshin_UID}`,
+          total_items: totalItems,
+        },
+      });
+
+      console.log('Data inserted successfully');
+      return res.json({ message: 'newData' });
+    } else {
+      console.log('No new data');
+      return res.json({ message: 'noNewData' });
+    }
+  } catch (errors) {
+    console.error('Fetch error:', errors);
+    return res.json({ message: errors });
+  }
+};
