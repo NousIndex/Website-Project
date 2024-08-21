09a547a025e34edc9fcd3f866da2257cfbf34bab
XiangHui <92633730+XiangHui556@users.noreply.github.com>
2024-08-21 23:57:04 +0800

    Update draw-import.js

--- api/draw-import.js
+++ api/draw-import.js
@@ -37,7 +37,7 @@ module.exports = async (req, res) => {
 
       while (loop) {
         const apiUrl =
-          'https://hk4e-api-os.hoyoverse.com/gacha_info/api/getGachaLog?authkey_ver=1&sign_type=2&auth_appid=webview_gacha&init_type=' +
+          'https://sg-hk4e-api.hoyoverse.com/gacha_info/api/getGachaLog?authkey_ver=1&sign_type=2&auth_appid=webview_gacha&init_type=' +
           banner +
           '&lang=en&authkey=' +
           authkey +
@@ -255,7 +255,7 @@ module.exports = async (req, res) => {
 
       while (loop) {
         const apiUrl =
-          'https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?authkey_ver=1&sign_type=2&auth_appid=webview_gacha&default_gacha_type=' +
+          'https://public-operation-hkrpg-sg.hoyoverse.com/common/gacha_record/api/getGachaLog?authkey_ver=1&sign_type=2&auth_appid=webview_gacha&default_gacha_type=' +
           banner +
           '&lang=en&authkey=' +
           authkey +
@@ -323,6 +323,222 @@ module.exports = async (req, res) => {
                   // case '400':
                   //   item.gacha_type = 'Character Event Wish - 2';
                   //   break;
+                  case '12':
+                    item.gacha_type = 'Light Cone Warp';
+                    break;
+                  default:
+                    item.gacha_type = 'Unknown';
+                }
+
+                if (item.gacha_type !== 'Unknown') {
+                  // Split the date and time string into its components
+                  const [datePart, timePart, ampm] = item.time.split(' ');
+
+                  // Split the date components into year, month, and day
+                  const [year, month, day] = datePart.split('-').map(Number);
+
+                  // Split the time components into hours, minutes, seconds, and AM/PM
+                  const [time] = timePart.split(' ');
+                  const [hours, minutes, seconds] = time.split(':').map(Number);
+
+                  // Create the Date object
+                  const dateTime = new Date(
+                    year,
+                    month - 1,
+                    day,
+                    hours,
+                    minutes,
+                    seconds
+                  );
+
+                  newDraws.push({
+                    StarRail_UID: String(item.uid),
+                    DrawID: String(item.id),
+                    DrawTime: dateTime,
+                    DrawType: String(item.gacha_type),
+                    Item_Name: String(item.name),
+                    Rarity: String(item.rank_type),
+                  });
+                }
+
+                // console.log(
+                //   `Item with DrawID ${item.id} does not exist in Genshin_Draw table.`
+                // );
+              }
+            } catch (error) {
+              console.error(
+                `Error checking item with DrawID ${item.id}:`,
+                error
+              );
+            }
+          }
+          endid = itemList[itemList.length - 1].id;
+          if (duplicateFound) {
+            if (banner === 2) {
+              banner = 11;
+              endid = '0';
+            } else if (banner === 11) {
+              banner = 12;
+              endid = '0';
+              // } else if (banner === 400) {
+              //   banner = 302;
+              //   endid = '0';
+            } else if (banner === 12) {
+              banner = 1;
+              endid = '0';
+            } else {
+              loop = false;
+            }
+          }
+        } else {
+          if (banner === 2) {
+            // get uid
+            banner = 11;
+            endid = '0';
+          } else if (banner === 11) {
+            banner = 12;
+            endid = '0';
+            // } else if (banner === 400) {
+            //   banner = 302;
+            //   endid = '0';
+          } else if (banner === 12) {
+            banner = 1;
+            endid = '0';
+          } else {
+            loop = false;
+          }
+        }
+      }
+      // console.log(newDraws);
+      // console.log(userID);
+
+      // Access the "Games_Users" collection
+      const gamesUsersCollection = database.collection('Games_Users');
+
+      // Upsert the document with the specified UID
+      await gamesUsersCollection.findOneAndUpdate(
+        { UID: userID }, // Filter condition
+        { $set: { StarRail_UID: starrail_uid } }, // Update operation
+        { upsert: true } // Options: upsert if not found, return the updated document
+      );
+
+      if (newDraws.length > 0) {
+        // Access the "StarRail_Draw" collection
+        const starRailDrawCollection = database.collection('StarRail_Draw');
+
+        // Insert multiple documents into the collection
+        await starRailDrawCollection.insertMany(newDraws);
+
+        // Calculate the total number of items in newDraws
+        const totalItems = newDraws.length;
+
+        // Access the "SummaryTable" collection
+        const summaryTableCollection = database.collection('SummaryTable');
+
+        // Upsert the document with the specified Game_UID
+        await summaryTableCollection.findOneAndUpdate(
+          { Game_UID: `StarRail-${newDraws[0].StarRail_UID}` }, // Filter condition
+          {
+            $inc: { total_items: totalItems }, // Update operation to increment total_items
+          },
+          { upsert: true } // Options: upsert if not found, return the updated document
+        );
+
+        console.log('Data inserted successfully');
+        return res.json({ message: 'newData' });
+      } else {
+        console.log('No new data');
+        return res.json({ message: 'noNewData' });
+      }
+    } catch (errors) {
+      console.error('Fetch error:', errors);
+      return res.json({ message: errors });
+    } finally {
+      await client.close();
+    }
+  } else if (game === 'zzz') {
+    // * should be done
+    //console.log('Starting ZZZ Draw Import API');
+    try {
+      // Connect the client to the server
+      await client.connect();
+      // Access the database
+      const database = client.db('NousIndex');
+      let endid = '0';
+      let banner = 2;
+      const authkey = req.query.authkey;
+      const userID = req.query.userID;
+      const newDraws = [];
+      let loop = true;
+      let zzz_uid = '';
+
+      while (loop) {
+        const apiUrl =
+          'https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?authkey_ver=1&sign_type=2&auth_appid=webview_gacha&default_gacha_type=' +
+          banner +
+          '&lang=en&authkey=' +
+          authkey +
+          '&game_biz=hkrpg_global&gacha_type=' +
+          banner +
+          '&page=1&size=20&end_id=' +
+          endid;
+
+        // Use the built-in fetch to make the request to Mihoyo API
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        // Parse the response body as JSON
+        const responseData = await response.json();
+        //console.log('Response data:', responseData);
+        if (responseData.retcode === -110) {
+          // Visit API too frequently
+          // Wait for 1 seconds before trying again
+          await setTimeout(100);
+          continue;
+        }
+        if (!responseData.data) {
+          // console.log(responseData)
+          return res.json({ message: responseData.message });
+        }
+        const itemList = responseData.data.list;
+        if (itemList.length > 0) {
+          // No more data
+
+          let duplicateFound = false;
+
+          for (const item of itemList) {
+            try {
+              zzz_uid = item.uid;
+              // Access the "ZZZ_Draw" collection
+              const zzzDrawCollection =
+                database.collection('ZZZ_Draw');
+
+              // Find the document with the specified DrawID
+              const existingItem = await zzzDrawCollection.findOne({
+                DrawID: item.id,
+              });
+
+              if (existingItem) {
+                // console.log(
+                //   `Item with DrawID ${item.id} exists in Genshin_Draw table.`
+                // );
+                duplicateFound = true;
+                //console.log('Duplicate found');
+                break; // Exit the loop if a duplicate is found
+              } else {
+                switch (item.gacha_type) {
+                  case '2':
+                    item.gacha_type = 'Departure Warp';
+                    break;
+                  case '1':
+                    item.gacha_type = 'Standard Warp';
+                    break;
+                  case '11':
+                    item.gacha_type = 'Character Warp';
+                    break;
                   case '12':
                     item.gacha_type = 'Light Cone Warp';
                     break;
